3238d721a9cbe276e6ae534148b5bb0f1616d1b5
Martin Fogliacco <fogliaccomartin3@gmail.com>
2023-01-23 01:18:03 -0300

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import spanish from './spain.png';
+import english from './united-kingdom.png';
+
+export {
+  download,
+  logo,
+  preview,
+  spanish,
+  english,
+};
